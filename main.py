861911910a228a0749d86aebbea40ae4fef12372
Jonathan Chng <jonathanchngyk@gmail.com>
2023-04-17 14:41:49 +0800

    Added SQLite Database

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    ranking = db.Column(db.Integer, unique=True, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+db.create_all()
+
 
 @app.route("/")
 def home():
